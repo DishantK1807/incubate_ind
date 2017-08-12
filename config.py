eac2d1b56ab3db3959a9ec6c0dced06c7fcf4bdc
SiddharthSudhakar <1.siddharth.sudhakar@gmail.com>
2017-08-13 01:52:33 +0530

    fix mysql env vars

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class BaseConfig(object):
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     #ENVIRONMENT = property(lambda self: self.__class__.__name__)
-    MYSQL_DB = os.environ.get("MYSQL_DB")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
-    MYSQL_USER = os.environ.get("MYSQL_USER")
-    MYSQL_HOST = os.environ.get("MYSQL_HOST")
-    MYSQL_CURSORCLASS = os.environ.get("MYSQL_CURSORCLASS")
+    MYSQL_DATABASE_DB = os.environ.get("MYSQL_DB")
+    MYSQL_DATABASE_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_DATABASE_USER = os.environ.get("MYSQL_USER")
+    MYSQL_DATABASE_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_DATABASE_CURSORCLASS = os.environ.get("MYSQL_CURSORCLASS")
 
 class Development(BaseConfig):
     """Default Flask configuration inherited by all environments. Use this for development environments."""
